ac4517b598637790e3bed6b89b596067c335e7d1
kalikichandu <kalikichandrasekhar@gmail.com>
2017-11-05 16:54:12 +0530

    Update Reading_text_files_to_DB_using_Pandas.py

--- Text_files_data_to_SQL_Server_using_Pandas/Reading_text_files_to_DB_using_Pandas.py
+++ Text_files_data_to_SQL_Server_using_Pandas/Reading_text_files_to_DB_using_Pandas.py
@@ -1,21 +1,28 @@
+import os
 import pandas as pd
 import pyodbc
 
-df = pd.read_csv('1_1.txt',sep=" ", header = None)
-df.columns = ["First Name","Last Name","Gender","Course","Department","Section"]
-print (df)
-print(df.iloc[1]['First Name'])
-i = 0
-# ccc = "insert into table_2 values ('1_1','%s',%s,getdate())" % (df.iloc[i]['mac'],df.iloc[i]['rssi'])
-# print(ccc)
-while i < len(df):
-    #print(type(df.iloc[i]['mac']))
-    #ccc = "insert into table_2 values('1_1','" + df.iloc[i]['mac'] + "'," + df.iloc[i]['rssi'] + ",getdate())"
-    #print(ccc)
-    query = "insert into data_student values ('%s','%s','%s','%s','%s','%s')" % (df.iloc[i]['First Name'], df.iloc[i]['Last Name'], df.iloc[i]['Gender'], df.iloc[i]['Course'], df.iloc[i]['Department'], df.iloc[i]['Section'])
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=COMPUTER\SQLEXPRESS;DATABASE=default; Trusted_Connection=yes;')
-    cursor = conn.cursor()
-    cursor.execute(query)
-    conn.commit()
-    #print(df.iloc[i]['rssi'])
-    i+=1
+directory_in_str = os.getcwd()
+for file in os.listdir(directory_in_str+r"\\chandu"):
+     filename = os.fsdecode(file)
+     #Looping in inner directory
+     innerDirectory = directory_in_str + r"\\chandu\\"+filename+r"\\"     
+     for file in os.listdir(innerDirectory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".txt"):
+            filename = os.path.join(innerDirectory, filename)
+            df = pd.read_csv(filename,sep=" ", header = None)
+            df.columns = ["First Name","Last Name","Gender","Course","Department","Section"]
+            print (df)
+            print(df.iloc[1]['First Name'])
+            i = 0
+            while i < len(df):
+                query = "insert into data_student values ('%s','%s','%s','%s','%s','%s')" % (df.iloc[i]['First Name'], df.iloc[i]['Last Name'], df.iloc[i]['Gender'], df.iloc[i]['Course'], df.iloc[i]['Department'], df.iloc[i]['Section'])
+                print(query)
+                conn = pyodbc.connect('DRIVER={SQL Server};SERVER=COMPUTER\SQLEXPRESS;DATABASE=default; Trusted_Connection=yes;')
+                cursor = conn.cursor()
+                cursor.execute(query)
+                conn.commit()
+                i+=1
+                conn.close()
+              
