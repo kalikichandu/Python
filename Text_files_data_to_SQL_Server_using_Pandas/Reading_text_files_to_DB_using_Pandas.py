3d07f26d37a63ad5cfa0d70664ba1ecf989416dc
kalikichandu <kalikichandrasekhar@gmail.com>
2017-11-05 16:57:34 +0530

    Update Reading_text_files_to_DB_using_Pandas.py

--- Text_files_data_to_SQL_Server_using_Pandas/Reading_text_files_to_DB_using_Pandas.py
+++ Text_files_data_to_SQL_Server_using_Pandas/Reading_text_files_to_DB_using_Pandas.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import pyodbc
 
+# Getting the current working directory
 directory_in_str = os.getcwd()
 for file in os.listdir(directory_in_str+r"\\chandu"):
      filename = os.fsdecode(file)
@@ -10,15 +11,15 @@ for file in os.listdir(directory_in_str+r"\\chandu"):
      for file in os.listdir(innerDirectory):
         filename = os.fsdecode(file)
         if filename.endswith(".txt"):
+            # Getting the filename
             filename = os.path.join(innerDirectory, filename)
             df = pd.read_csv(filename,sep=" ", header = None)
-            df.columns = ["First Name","Last Name","Gender","Course","Department","Section"]
-            print (df)
-            print(df.iloc[1]['First Name'])
+            df.columns = ["First Name","Last Name","Gender","Course","Department","Section"]        
             i = 0
             while i < len(df):
-                query = "insert into data_student values ('%s','%s','%s','%s','%s','%s')" % (df.iloc[i]['First Name'], df.iloc[i]['Last Name'], df.iloc[i]['Gender'], df.iloc[i]['Course'], df.iloc[i]['Department'], df.iloc[i]['Section'])
-                print(query)
+                # SQL Query to Insert Data
+                query = "insert into data_student values ('%s','%s','%s','%s','%s','%s')" % (df.iloc[i]['First Name'], df.iloc[i]['Last Name'], df.iloc[i]['Gender'], df.iloc[i]['Course'], df.iloc[i]['Department'], df.iloc[i]['Section'])        
+                # SQL Connection String
                 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=COMPUTER\SQLEXPRESS;DATABASE=default; Trusted_Connection=yes;')
                 cursor = conn.cursor()
                 cursor.execute(query)
