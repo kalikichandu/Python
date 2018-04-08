a23c50c7a9fa78cfefe9fe743d975cf7a36e2d3c
chandu <kalikichandrasekhar@gmail.com>
2018-04-08 19:18:14 +0530

    Adding new file

---
+++ Challanges/Sort even-placed elements in increasing and odd-placed in decreasing order.py
@@ -0,0 +1,24 @@
+#Sort even-placed elements in increasing and odd-placed in decreasing order
+
+input = [3,1,2,4,5,9,13,14,12]
+
+print("Input: "+str(input))
+
+evenPlacedElements = []
+
+oddPlacedElements = []
+
+output = []
+
+for x,value in zip(range(len(input)),input):
+    if x == 0 or x % 2 == 0:
+        evenPlacedElements.append(value)
+    else:
+        oddPlacedElements.append(value)
+
+print("Even Placed Elements in Increasing Order: "+ str(sorted(evenPlacedElements)))
+
+print("Odd Placed Elements in Decreasing Order"+str(sorted(oddPlacedElements,reverse=True)))
+print("Output: "+str(sorted(evenPlacedElements) + sorted(oddPlacedElements,reverse=True)))
+
+
