9dc7892f714c5daf0c5fe4f9d91ca092e6635aa2
chandu <kalikichandrasekhar@gmail.com>
2018-04-08 22:42:07 +0530

    Removed commented code

--- Challanges/Number to Text Converter.py
+++ Challanges/Number to Text Converter.py
@@ -2,13 +2,11 @@ dict = {1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",
         ,13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen",20:"twenty",
         30:"thirty",40:"fourty",50:"fifty",60:"sixty",70:"seventy",80:"eighty",90:"ninety"
         }
-# print(type(dict))
-# print(dict[1])
 
 digitToBeConverted = 99345678917510
 i = 1
 digitList = str(digitToBeConverted)
-# print(digitList[0])
+
 output = ""
 j = len(digitList)-1
 while (i <= len(digitList) and j >= 0):
@@ -29,19 +27,15 @@ while (i <= len(digitList) and j >= 0):
 
             elif key == int(digitList[len(digitList)-j-1]+'0'):
                 output = output + dict[key] + " "
-                # print(output)
-                # j -=1
                 break
 
 
         elif str(key) == digitList[len(digitList)-j-1]:
             if j == 2 or j == 9:
                 output = output + dict[key]+" hundred "
-                # j -= 1
                 break
             elif j == 0:
                 output = output + dict[key] +" "
-                # print(output)
             elif j == 3 or j == 10:
                 output = output + dict[key] + " thousand "
             elif j == 5 or j == 12:
@@ -49,9 +43,6 @@ while (i <= len(digitList) and j >= 0):
             elif j == 7:
                 output = output + dict[key] + " crore "
     j -= 1
-
-
     i +=1
-    # print(i)
-    # print(j)
+
 print(output)
